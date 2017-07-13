65f550c57cd4d9a4172fdc77c13eb8146f09de67
xiankun.geng <ge2x3k2@gmail.com>
2017-07-13 10:16:15 +0800

    [update] samurai-champloo.rb sha256

--- samurai-champloo.rb
+++ samurai-champloo.rb
@@ -7,6 +7,7 @@ class SamuraiChamploo < Formula
   homepage "https://github.com/gengxiankun/samurai-champloo"
   url "https://github.com/gengxiankun/samurai-champloo/archive/master.tar.gz"
   version "2.0"
+  sha256 "0c256b52a76e8a6e66c963dcbf52c0ca4d4866c87cd52f0ecd5bc1b99ae3a029"
 
   def install
     bin.install "ploo"
