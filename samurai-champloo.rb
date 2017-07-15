cd4e394947bae3d1d1a36518cd085e3292c85b62
xiankun.geng <ge2x3k2@gmail.com>
2017-07-15 13:45:46 +0800

    Update samurai-champloo.rb

--- samurai-champloo.rb
+++ samurai-champloo.rb
@@ -6,7 +6,7 @@ class SamuraiChamploo < Formula
   desc "ploo(类alias管理器) BASED ON MacOS."
   homepage "https://github.com/gengxiankun/samurai-champloo"
   url "https://github.com/gengxiankun/samurai-champloo/archive/master.tar.gz"
-  version "2.0"
+  version "2.1"
   sha256 "0c256b52a76e8a6e66c963dcbf52c0ca4d4866c87cd52f0ecd5bc1b99ae3a029"
 
   def install
