ff151bacb58b976e5a8a06197d20bc8851dadc6c
xiankun.geng <ge2x3k2@gmail.com>
2017-07-21 11:52:01 +0800

    Create ploo.rb

---
+++ ploo.rb
@@ -0,0 +1,11 @@
+class Ploo < Formula
+  desc "ploo(类alias管理器) BASED ON MacOS."
+  homepage "https://github.com/gengxiankun/ploo"
+  url "https://github.com/gengxiankun/ploo/archive/master.tar.gz"
+  version "2.2"
+  sha256 ""
+
+  def install
+    bin.install "ploo"
+  end
+end
