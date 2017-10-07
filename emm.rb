2d111ba81d1602736ee668d353a3ca0fab55772f
x1ankun <ge2x3k2@gmail.com>
2017-10-07 04:42:17 -0500

    Update emm.rb

--- emm.rb
+++ emm.rb
@@ -2,7 +2,7 @@ class Emm < Formula
   desc "emm(类alias管理器) BASED ON MacOS."
   homepage "https://github.com/gengxiankun/emm"
   url "https://github.com/gengxiankun/emm/archive/master.tar.gz"
-  version "v3.0.1"
+  version "v3.0.2"
   sha256 ""
 
   def install
