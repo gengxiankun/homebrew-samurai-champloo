08737328492c1fe70dd9a25efa6f31e644c23de0
x1ankun <ge2x3k2@gmail.com>
2017-10-07 04:35:20 -0500

    Update ploo.rb

--- ploo.rb
+++ ploo.rb
@@ -1,11 +1,11 @@
-class Ploo < Formula
-  desc "ploo(类alias管理器) BASED ON MacOS."
-  homepage "https://github.com/gengxiankun/ploo"
-  url "https://github.com/gengxiankun/ploo/archive/master.tar.gz"
-  version "2.2"
+class Emm < Formula
+  desc "emm(类alias管理器) BASED ON MacOS."
+  homepage "https://github.com/gengxiankun/emm"
+  url "https://github.com/gengxiankun/emm/archive/master.tar.gz"
+  version "v3.0.1"
   sha256 ""
 
   def install
-    bin.install "ploo"
+    bin.install "emm"
   end
 end
