184d4ab7f23234e6c73feaab960a66e835601e32
x1ankun <ge2x3k2@gmail.com>
2017-11-25 21:03:55 +0800

    Update and rename emm.rb to fcb.rb

---
+++ fcb.rb
@@ -0,0 +1,11 @@
+class Fcb < Formula
+  desc "freezecombo BASED ON MacOS."
+  homepage "https://github.com/gengxiankun/fcb"
+  url "https://github.com/gengxiankun/fcb/archive/master.tar.gz"
+  version "v4.0.0"
+  sha256 ""
+
+  def install
+    bin.install "fcb"
+  end
+end
