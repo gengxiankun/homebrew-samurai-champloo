f14c376527ac1b1f722e2048d29f4de31f6f19d7
xiankun Geng <ge2x3k2@gmail.com>
2017-12-12 10:43:37 +0800

    Update fcb.rb

--- fcb.rb
+++ fcb.rb
@@ -2,7 +2,7 @@ class Fcb < Formula
   desc "freezecombo BASED ON MacOS."
   homepage "https://github.com/gengxiankun/fcb"
   url "https://github.com/gengxiankun/fcb/archive/master.tar.gz"
-  version "v5.1.1"
+  version "v5.1.2"
   sha256 ""
 
   def install
