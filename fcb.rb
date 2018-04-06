7523d24391437ec057eb48a0c5d331d35a72a960
xiankun Geng <ge2x3k2@gmail.com>
2018-04-07 00:45:54 +0800

    Update fcb.rb

--- fcb.rb
+++ fcb.rb
@@ -1,11 +1,11 @@
-class Fcb < Formula
-  desc "freezecombo BASED ON MacOS."
-  homepage "https://github.com/gengxiankun/fcb"
-  url "https://github.com/gengxiankun/fcb/archive/master.tar.gz"
-  version "v5.1.3"
+class Chair < Formula
+  desc "chair BASED ON MacOS."
+  homepage "https://github.com/gengxiankun/chair"
+  url "https://github.com/gengxiankun/chair/archive/master.tar.gz"
+  version "5.2.0"
   sha256 ""
 
   def install
-    bin.install "fcb"
+    bin.install "chair"
   end
 end
