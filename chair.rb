d2939ccfa218a1bb7537efec64ddb45903988c0e
xiankun.geng <ge2x3k2@gmail.com>
2018-05-19 07:40:12 +0800

    Update chair.rb

--- chair.rb
+++ chair.rb
@@ -1,8 +1,8 @@
 class Chair < Formula
   desc "chair BASED ON MacOS."
   homepage "https://github.com/gengxiankun/chair"
-  url "https://github.com/gengxiankun/chair/archive/5.3.0.tar.gz"
-  version "5.3.0"
+  url "https://github.com/gengxiankun/chair/archive/6.0.tar.gz"
+  version "6.0"
   sha256 ""
 
   def install
