d55eddfa94adcf75559e276698bdc01a963bff33
耿 贤坤 <gengxiankun@126.com>
2018-11-26 18:02:35 +0800

    Update ssh-am.rb

--- ssh-am.rb
+++ ssh-am.rb
@@ -1,4 +1,4 @@
-class SshAccountManagement < Formula
+class SshAm < Formula
   desc "ssh-am BASED ON MacOS."
   homepage "https://github.com/gengxiankun/ssh-am"
   url "https://github.com/gengxiankun/ssh-am/releases/tag/v1.0.0"
@@ -6,6 +6,6 @@ class SshAccountManagement < Formula
   sha256 ""
 
   def install
-    bin.install "SshAccountManagement"
+    bin.install "ssh-am"
   end
 end
