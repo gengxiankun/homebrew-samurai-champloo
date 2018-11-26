204558f376d81faa0a2b291da208658f68d5b79f
耿 贤坤 <gengxiankun@126.com>
2018-11-26 17:54:09 +0800

    Update and rename dgjj.rb to ssh-am.rb

---
+++ ssh-am.rb
@@ -0,0 +1,11 @@
+class ssh-am < Formula
+  desc "ssh-am BASED ON MacOS."
+  homepage "https://github.com/gengxiankun/ssh-am"
+  url "https://github.com/gengxiankun/ssh-am/releases/tag/v1.0.0"
+  version "1.0.0"
+  sha256 ""
+
+  def install
+    bin.install "ssh-am"
+  end
+end
