47637a7d9386d61b17b4ef033787ab0afd898b4c
耿 贤坤 <gengxiankun@126.com>
2019-11-01 13:04:38 +0800

    Create opssh.rb

---
+++ opssh.rb
@@ -0,0 +1,11 @@
+class SshAm < Formula
+  desc "OPSSH"
+  homepage "https://github.com/gengxiankun/opssh"
+  url "https://github.com/gengxiankun/opssh/archive/v0.1.tar.gz"
+  version "0.1"
+  sha256 ""
+
+  def install
+    bin.install "opssh"
+  end
+end
