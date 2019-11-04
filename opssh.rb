4e9f87c1313a51fb979b23ed6d03504c3c642fa9
耿 贤坤 <gengxiankun@126.com>
2019-11-04 21:11:13 +0800

    Update opssh.rb

--- opssh.rb
+++ opssh.rb
@@ -1,8 +1,8 @@
 class Opssh < Formula
   desc "OPSSH"
   homepage "https://github.com/gengxiankun/opssh"
-  url "https://github.com/gengxiankun/opssh/archive/v0.1.tar.gz"
-  version "0.1"
+  url "https://github.com/gengxiankun/opssh/archive/v0.2.tar.gz"
+  version "0.2"
   sha256 ""
 
   def install
