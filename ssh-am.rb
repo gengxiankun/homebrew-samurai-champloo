fb33391842b7140e2450f459be51af1163d83d50
耿 贤坤 <gengxiankun@126.com>
2020-02-04 11:17:18 +0800

    Update ssh-am.rb

--- ssh-am.rb
+++ ssh-am.rb
@@ -1,8 +1,8 @@
 class SshAm < Formula
   desc "ssh-am BASED ON MacOS."
   homepage "https://github.com/gengxiankun/ssh-am"
-  url "https://github.com/gengxiankun/ssh-am/archive/v1.2.2.tar.gz"
-  version "1.2.2"
+  url "https://github.com/gengxiankun/ssh-am/archive/v1.2.3.tar.gz"
+  version "1.2.3"
   sha256 ""
 
   def install
